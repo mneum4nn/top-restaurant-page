41e7cbdfdfbaad159f87a56f40eda153eb80a59c
mneum4nn <n3um4nn@proton.me>
2023-12-14 15:53:38 -0600

    Create homepage through modules

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import _ from 'lodash';
 import { createHeaderBox } from './boxes';
 import { createTxtBox } from './boxes';
-import { createPage } from './delivery';
+import { createForm } from './delivery';
 import background from './funfetti.jpg';
+
 console.log("I'm working");
 
-function determineTab (tabID){
+function tabHandler (tabID){
+    let newPage;
 
+    //empty out previous page
+    while (content.firstChild) {
+        content.removeChild(parentElement.firstChild);
+    }
+    
+    switch (tabID){
+        case 'delivery':
+            newPage = createForm ();
+            break;
+        case 'menu':
+            newPage = createMenu ();
+            break;
+        case 'about':
+            newPage = createHomePage();
+            break;
+    }
+    return newPage;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const content = document.querySelector('.content');
         content.style.display= 'flex';
         content.style.flexDirection ='column';
-
-    console.log(content);
-
-    const pleasance = createHeaderBox('pleasance');
-        pleasance.style.backgroundImage = `url(${background})`;
-        pleasance.style.backgroundSize = 'cover';
-    const aboutUs = createTxtBox('About Us', "We believe in locally sourced magic, and each ingredient included on our menu items was personally cared for by a warlock");
-    const hours = createTxtBox('Visit Us','Monday to Friday \n 1AM - 3PM \n Saturdays \n 4PM - 3AM');
-
-    content.appendChild(pleasance);
-    content.appendChild(aboutUs);
-    content.appendChild(hours);
-
-    const delivForm = createPage();
-    content.appendChild(delivForm);
-
-
+    const homepage = createHomePage()
+    content.appendChild(homepage);
 });
-
