75887eb0f4ced3dc8c7688002f7673ee5163173c
mneum4nn <n3um4nn@proton.me>
2023-12-14 15:53:06 -0600

    Separate homepage function from index

---
+++ src/home.js
@@ -0,0 +1,16 @@
+function createHomePage(){
+    const container = document.createElement('div');
+        container.id ='about';
+    const pleasance = createHeaderBox('pleasance');
+        pleasance.style.backgroundImage = `url(${background})`;
+        pleasance.style.backgroundSize = 'cover';
+    const aboutUs = createTxtBox('About Us', "We believe in locally sourced magic, and each ingredient included on our menu items was personally cared for by a warlock");
+    const hours = createTxtBox('Visit Us','Monday to Friday \n 1AM - 3PM \n Saturdays \n 4PM - 3AM');
+
+    container.appendChild(pleasance);
+    container.appendChild(aboutUs);
+    container.appendChild(hours);
+
+    return container; 
+}
+
